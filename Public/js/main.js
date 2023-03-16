1ccc6fdf775d2ed26aff49726c133b5f4e146995
arifkhan676 <arifulhasan5500@gmail.com>
2023-03-17 03:51:32 +0600

    user send data

--- Public/js/main.js
+++ Public/js/main.js
@@ -1,5 +1,37 @@
 const socket = io();
 
-socket.on('message',message =>{  //here I get it from server
+const chatFrom = document.getElementById("chat-form");
+const chatMessage = document.querySelector(".chat-messages");
+
+socket.on('message',message =>{  //here I get it from server to my console log
     console.log(message);
+    OutputMessage(message); //this function will show the msg to the index page
+    //scroll down when everytime gets msg
+    chatMessage.scrollTop = chatMessage.scrollHeight;
 })
+
+chatFrom.addEventListener('submit',(e)=>{
+    e.preventDefault(); // prevent the default behavior
+    //get a msg text
+    const msg = e.target.elements.msg.value;
+   // emmiting a msg to server
+    socket.emit('chatMessage',msg);
+
+    //after passed input should blank
+    e.target.elements.msg.value='';
+    e.target.elements.msg.focus();
+
+})
+
+//Output message to DOM
+
+function OutputMessage(message){ //this function will show the msg to the index page
+   const div = document.createElement('div');
+   div.classList.add('message'); //all the classes inside html
+   div.innerHTML = `<p class="meta">Brad <span>9:12pm</span></p>
+   <p class="text">
+       ${message}
+   </p> `;
+   document.querySelector(".chat-messages").appendChild(div);
+
+}
--- server.js
+++ server.js
@@ -2,25 +2,31 @@ const path = require ('path'); //path helps to access our public folder to the s
 const http = require('http');
 const express = require('express');
 const socketio=require('socket.io');
+const formateMessage = require('./ultis/messages');
 
 const app = express();
 const server = http.createServer(app); //passing app to the server
 const io=socketio(server); 
 
+const botName = 'ChatcordBot';
 //Set static folder 
 app.use(express.static(path.join(__dirname,'public'))); //it access our public folder to the server
 
 //Run when a client connects 
 
 io.on('connection',socket => {
-    socket.emit('message','Welcome to Chatcord'); // socket emit message ta client side theke server e pathay & its for single user
+    socket.emit('message',formateMessage(botName,'Welcome to Chatcord !')); // socket emit message ta client side theke server e pathay & its for single user
     //console.log('New WS Connection...'); check the server
 
-  socket.broadcast.emit('message','A user has joined'); // broadcast when a user connect & use for all of the clients
+  socket.broadcast.emit('message',formateMessage(botName,'A user has joined')); // broadcast when a user connect & use for all of the clients
   //when client disconnect
   socket.on('disconnect',()=>{
-    io.emit('message','A user has left from chat'); // io.emit will known everyone that user has left
-  })
+    io.emit('message',formateMessage(botName,'A user has left from chat')); // io.emit will known everyone that user has left
+  });
+  //listen for chatMessage from client side
+  socket.on('chatMessage', msg => {
+    io.emit('message',formateMessage('User',msg)); // io.emit listen for everyone
+  });
 })
 
 const PORT = 3000 || process.env.PORT; // use 3000 or port local environment
