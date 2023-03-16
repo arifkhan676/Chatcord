df22f4c2179d70091d496106c5d3ef808a8ecd4d
arifkhan676 <arifulhasan5500@gmail.com>
2023-03-17 02:18:20 +0600

    pass the data

--- Public/js/main.js
+++ Public/js/main.js
@@ -1,2 +1,5 @@
 const socket = io();
 
+socket.on('message',message =>{  //here I get it from server
+    console.log(message);
+})
--- server.js
+++ server.js
@@ -4,15 +4,23 @@ const express = require('express');
 const socketio=require('socket.io');
 
 const app = express();
-const server = http.createServer(app);
-const io=socketio(server);
-//Set static folder 
+const server = http.createServer(app); //passing app to the server
+const io=socketio(server); 
 
+//Set static folder 
 app.use(express.static(path.join(__dirname,'public'))); //it access our public folder to the server
 
 //Run when a client connects 
+
 io.on('connection',socket => {
-    console.log('New WS Connection...');
+    socket.emit('message','Welcome to Chatcord'); // socket emit message ta client side theke server e pathay & its for single user
+    //console.log('New WS Connection...'); check the server
+
+  socket.broadcast.emit('message','A user has joined'); // broadcast when a user connect & use for all of the clients
+  //when client disconnect
+  socket.on('disconnect',()=>{
+    io.emit('message','A user has left from chat'); // io.emit will known everyone that user has left
+  })
 })
 
 const PORT = 3000 || process.env.PORT; // use 3000 or port local environment
